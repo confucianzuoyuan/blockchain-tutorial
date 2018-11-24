a516bc2892a8dfe01a58207af81ef11d80abc597
confucianzuoyuan <zuoyuantc@126.com>
2018-11-24 11:25:21 +0800

    fix

--- 超级账本教程/writing-code/atguigucar/invoke.js
+++ 超级账本教程/writing-code/atguigucar/invoke.js
@@ -82,38 +82,33 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 		var sendPromise = channel.sendTransaction(request);
 		promises.push(sendPromise);
 
-		// get an eventhub once the fabric client has a user assigned. The user
-		// is required bacause the event registration must be signed
+        // 当为fabric客户端指派了一个用户的时候, 初始化一个event_hub。
 		let event_hub = channel.newChannelEventHub(peer);
 
-		// using resolve the promise so that result status may be processed
-		// under the then clause rather than having the catch clause process
-		// the status
 		let txPromise = new Promise((resolve, reject) => {
+            // 定义了一个名为handle的定时器
 			let handle = setTimeout(() => {
 				event_hub.unregisterTxEvent(transaction_id_string);
 				event_hub.disconnect();
-				resolve({event_status : 'TIMEOUT'}); //we could use reject(new Error('Trnasaction did not complete within 30 seconds'));
+				resolve({event_status : 'TIMEOUT'});
 			}, 3000);
 			event_hub.registerTxEvent(transaction_id_string, (tx, code) => {
-				// this is the callback for transaction event status
-				// first some clean up of event listener
+                // 这个回调函数用来处理交易事件的状态
+                // 首先清除掉定时器, 前提是在3秒以内回调函数被调用
 				clearTimeout(handle);
 
-				// now let the application know what happened
 				var return_status = {event_status : code, tx_id : transaction_id_string};
 				if (code !== 'VALID') {
-					console.error('The transaction was invalid, code = ' + code);
-					resolve(return_status); // we could use reject(new Error('Problem with the tranaction, event status ::'+code));
+					console.error('交易无效，错误码为: ' + code);
+					resolve(return_status);
 				} else {
-					console.log('The transaction has been committed on peer ' + event_hub.getPeerAddr());
+					console.log('交易已经提交到节点，节点地址是: ' + event_hub.getPeerAddr());
 					resolve(return_status);
 				}
 			}, (err) => {
-				//this is the callback if something goes wrong with the event registration or processing
-				reject(new Error('There was a problem with the eventhub ::'+err));
+				reject(new Error('eventhub出问题了 ::'+err));
 			},
-				{disconnect: true} //disconnect when complete
+				{disconnect: true} // 完成以后断开连接
 			);
 			event_hub.connect();
 
@@ -121,25 +116,23 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 		promises.push(txPromise);
 
         // 执行所有的promise，等待所有的promise执行完
+        // 这里要注意Promise.all强制promises数组中的promise顺序执行
 		return Promise.all(promises);
 	} else {
-		console.error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
-		throw new Error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+		throw new Error('发送交易失败. Response null or status is not 200. exiting...');
 	}
 }).then((results) => {
-	console.log('Send transaction promise and event listener promise have completed');
-	// check the results in the order the promises were added to the promise all list
 	if (results && results[0] && results[0].status === 'SUCCESS') {
-		console.log('Successfully sent transaction to the orderer.');
+		console.log('成功将交易发送至orderer排序节点');
 	} else {
-		console.error('Failed to order the transaction. Error code: ' + results[0].status);
+		console.error('交易排序错误，错误码为: ' + results[0].status);
 	}
 
 	if(results && results[1] && results[1].event_status === 'VALID') {
-		console.log('Successfully committed the change to the ledger by the peer');
+		console.log('成功将改变commit到账本');
 	} else {
-		console.log('Transaction failed to be committed to the ledger due to ::'+results[1].event_status);
+		console.log('交易未被commit到账本，原因是::'+results[1].event_status);
 	}
 }).catch((err) => {
-	console.error('Failed to invoke successfully :: ' + err);
+	console.error('invoke失败 :: ' + err);
 });
