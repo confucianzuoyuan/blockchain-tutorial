5008235232d535e7225e5e316a3258e8fb6bf9fa
confucianzuoyuan <zuoyuantc@126.com>
2018-11-27 14:07:37 +0800

    fix

--- 超级账本教程/项目/汽车项目/README.md
+++ 超级账本教程/项目/汽车项目/README.md
@@ -73,8 +73,11 @@ CC_SRC_PATH=github.com/atguigucar/go
 ```
 
 ```
+# 安装链代码
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.atguigu.com/users/Admin@org1.atguigu.com/msp" cli peer chaincode install -n atguigucar -v 1.0 -p "$CC_SRC_PATH" -l "$LANGUAGE"
+# 初始化链代码
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.atguigu.com/users/Admin@org1.atguigu.com/msp" cli peer chaincode instantiate -o orderer.atguigu.com:7050 -C atguiguchannel -n atguigucar -l "$LANGUAGE" -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+# 使用invode初始化账本
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.atguigu.com/users/Admin@org1.atguigu.com/msp" cli peer chaincode invoke -o orderer.atguigu.com:7050 -C atguiguchannel -n atguigucar -c '{"function":"initLedger","Args":[""]}'
 ```
 
--- 超级账本教程/项目/汽车项目/registerUser.js
+++ 超级账本教程/项目/汽车项目/registerUser.js
@@ -39,13 +39,13 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
     return fabric_ca_client.register({enrollmentID: 'zuoyuan', affiliation: 'org1.department1',role: 'client'}, admin_user);
 }).then((secret) => {
     // next we need to enroll the user with CA server
-    console.log('Successfully registered user1 - secret:'+ secret);
+    console.log('Successfully registered zuoyuan - secret:'+ secret);
 
-    return fabric_ca_client.enroll({enrollmentID: 'user1', enrollmentSecret: secret});
+    return fabric_ca_client.enroll({enrollmentID: 'zuoyuan', enrollmentSecret: secret});
 }).then((enrollment) => {
-  console.log('Successfully enrolled member user "user1" ');
+  console.log('Successfully enrolled member user "zuoyuan" ');
   return fabric_client.createUser(
-     {username: 'user1',
+     {username: 'zuoyuan',
      mspid: 'Org1MSP',
      cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
      });
@@ -54,7 +54,7 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 
      return fabric_client.setUserContext(member_user);
 }).then(()=>{
-     console.log('User1 was successfully registered and enrolled and is ready to interact with the fabric network');
+     console.log('zuoyuan was successfully registered and enrolled and is ready to interact with the fabric network');
 
 }).catch((err) => {
     console.error('Failed to register: ' + err);
